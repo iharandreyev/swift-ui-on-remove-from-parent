ed71a6eeeb192710fd75ad560c2d386afd9918ee
Ihar Andreyeu <iandreyeu@scnsoft.com>
2025-03-08 03:38:07 +0400

    Import `IssueReporting`

--- Package.swift
+++ Package.swift
@@ -4,22 +4,39 @@
 import PackageDescription
 
 let package = Package(
-    name: "OnRemoveFromParent",
-    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)],
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "OnRemoveFromParent",
-            targets: ["OnRemoveFromParent"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "OnRemoveFromParent"),
-        .testTarget(
-            name: "OnRemoveFromParentTests",
-            dependencies: ["OnRemoveFromParent"]
-        ),
-    ]
+  name: "OnRemoveFromParent",
+  platforms: [
+    .macOS(.v10_15),
+    .iOS(.v13),
+    .tvOS(.v13),
+    .watchOS(.v6)
+  ],
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "OnRemoveFromParent",
+      targets: [
+        "OnRemoveFromParent",
+      ]
+    ),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.2.2"),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+      name: "OnRemoveFromParent",
+      dependencies: [
+        .product(name: "IssueReporting", package: "xctest-dynamic-overlay"),
+      ]
+    ),
+    .testTarget(
+      name: "OnRemoveFromParentTests",
+      dependencies: [
+        "OnRemoveFromParent",
+      ]
+    ),
+  ]
 )
